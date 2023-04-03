679143ad1b2330bea1d6bbdbbaa07c58f81b7488
amirhossein-k <amirhossein.k787878@gmail.com>
2023-04-03 20:14:55 +0430

    ee

--- app.js
+++ app.js
@@ -1,40 +1,64 @@
 const priceTitle = document.querySelectorAll('.typeprice ul li a')
+const section = document.querySelectorAll('section')
+const alink = document.querySelectorAll('header .navlink')
 const headerMobile = document.querySelector('.header-mobile')
 const headerLarge = document.querySelector('.header_large')
+const close = document.querySelector('.header-mobile .collapse .box-my .close')
+const navbar = document.querySelector('#navbarTogglerDemo02')
+const contact = document.querySelector('.contact .row')
 
-console.log(headerLarge.style.display)
+
+
 const heightOutput = document.querySelector("#height");
 const widthOutput = document.querySelector("#width");
+
+
 
 function updateSize() {
  
     if(window.innerWidth<1149){
         headerMobile.style.display='block'
         headerLarge.style.display='none'
-    }else{
+        navbar.classList.remove('show')
+        
+      
+    }
+    else if(window.innerWidth<993){
+        contact.style.gap="15px"
+        contact.style.padding= "50px !important";
+        
+    }
+    else{
         headerMobile.style.display='none'
         headerLarge.style.display='block'
+        contact.style.gap="0"
     }
 }
 
 updateSize();
 window.addEventListener("resize", updateSize);
+close.addEventListener('click',()=>{
+    console.log(navbar)
+    navbar.classList.remove('show')
+})
 
+alink.forEach((item,index)=>{
+    item.addEventListener('click',(e)=>{
+        e.preventDefault()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        section.forEach((se,indexx)=>{
+        
+                if(se.className.toUpperCase()===item.lastChild.previousSibling.innerText){
+                    se.scrollIntoView({
+                        block:'start',
+                        behavior:'smooth',
+                        inline:'start'
+                    })
+                }
+            })
+    })
+  })
+  
   priceTitle.forEach((item,index)=>{
     
     item.addEventListener('click',(e)=>{
